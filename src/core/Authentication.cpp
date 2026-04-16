e7960a1e5726607dca74d0e76afecc34a0328abe
bartoszruta26-droid <bartosz.ruta26@gmail.com>
2026-04-16 19:19:27 +0200

    Harden auth JSON user loading and add parser regressions

--- src/core/Authentication.cpp
+++ src/core/Authentication.cpp
@@ -1,8 +1,244 @@
 #include "core/Authentication.hpp"
 #include <algorithm>
+#include <cctype>
 #include <cstdio>
 
 namespace biofeedback {
+namespace {
+
+std::string escapeJsonString(const std::string& input)
+{
+    std::string output;
+    output.reserve(input.size());
+
+    for (const char c : input) {
+        switch (c) {
+            case '"': output += "\\\""; break;
+            case '\\': output += "\\\\"; break;
+            case '\b': output += "\\b"; break;
+            case '\f': output += "\\f"; break;
+            case '\n': output += "\\n"; break;
+            case '\r': output += "\\r"; break;
+            case '\t': output += "\\t"; break;
+            default: output += c; break;
+        }
+    }
+
+    return output;
+}
+
+std::string unescapeJsonString(const std::string& input)
+{
+    std::string output;
+    output.reserve(input.size());
+
+    for (size_t i = 0; i < input.size(); ++i) {
+        if (input[i] == '\\' && i + 1 < input.size()) {
+            switch (input[i + 1]) {
+                case '"': output += '"'; ++i; break;
+                case '\\': output += '\\'; ++i; break;
+                case 'b': output += '\b'; ++i; break;
+                case 'f': output += '\f'; ++i; break;
+                case 'n': output += '\n'; ++i; break;
+                case 'r': output += '\r'; ++i; break;
+                case 't': output += '\t'; ++i; break;
+                default: output += input[i]; break;
+            }
+        } else {
+            output += input[i];
+        }
+    }
+
+    return output;
+}
+
+size_t findStringTerminator(const std::string& text, const size_t start)
+{
+    for (size_t i = start; i < text.size(); ++i) {
+        if (text[i] != '"') {
+            continue;
+        }
+
+        size_t backslashes = 0;
+        for (size_t j = i; j > 0 && text[j - 1] == '\\'; --j) {
+            ++backslashes;
+        }
+
+        if (backslashes % 2 == 0) {
+            return i;
+        }
+    }
+
+    return std::string::npos;
+}
+
+std::string extractJsonStringField(const std::string& jsonText, const std::string& key)
+{
+    const std::string searchKey = "\"" + key + "\"";
+    const size_t keyPos = jsonText.find(searchKey);
+    if (keyPos == std::string::npos) {
+        return "";
+    }
+
+    const size_t colonPos = jsonText.find(':', keyPos + searchKey.size());
+    if (colonPos == std::string::npos) {
+        return "";
+    }
+
+    const size_t quotePos = jsonText.find('"', colonPos + 1);
+    if (quotePos == std::string::npos) {
+        return "";
+    }
+
+    const size_t endQuotePos = findStringTerminator(jsonText, quotePos + 1);
+    if (endQuotePos == std::string::npos || endQuotePos <= quotePos) {
+        return "";
+    }
+
+    return unescapeJsonString(jsonText.substr(quotePos + 1, endQuotePos - quotePos - 1));
+}
+
+bool extractJsonBoolField(const std::string& jsonText, const std::string& key, const bool defaultValue)
+{
+    const std::string searchKey = "\"" + key + "\"";
+    const size_t keyPos = jsonText.find(searchKey);
+    if (keyPos == std::string::npos) {
+        return defaultValue;
+    }
+
+    size_t pos = jsonText.find(':', keyPos + searchKey.size());
+    if (pos == std::string::npos) {
+        return defaultValue;
+    }
+
+    ++pos;
+    while (pos < jsonText.size() && std::isspace(static_cast<unsigned char>(jsonText[pos]))) {
+        ++pos;
+    }
+
+    if (jsonText.compare(pos, 4, "true") == 0) {
+        return true;
+    }
+    if (jsonText.compare(pos, 5, "false") == 0) {
+        return false;
+    }
+
+    return defaultValue;
+}
+
+std::string findUsersArray(const std::string& jsonContent)
+{
+    const std::string key = "\"users\"";
+    const size_t keyPos = jsonContent.find(key);
+    if (keyPos == std::string::npos) {
+        return "";
+    }
+
+    const size_t bracketStart = jsonContent.find('[', keyPos + key.size());
+    if (bracketStart == std::string::npos) {
+        return "";
+    }
+
+    bool inString = false;
+    int depth = 0;
+    for (size_t i = bracketStart; i < jsonContent.size(); ++i) {
+        const char c = jsonContent[i];
+
+        if (c == '"') {
+            size_t backslashes = 0;
+            for (size_t j = i; j > 0 && jsonContent[j - 1] == '\\'; --j) {
+                ++backslashes;
+            }
+            if (backslashes % 2 == 0) {
+                inString = !inString;
+            }
+        }
+
+        if (inString) {
+            continue;
+        }
+
+        if (c == '[') {
+            ++depth;
+        } else if (c == ']') {
+            --depth;
+            if (depth == 0) {
+                return jsonContent.substr(bracketStart, i - bracketStart + 1);
+            }
+        }
+    }
+
+    return "";
+}
+
+std::vector<std::string> splitJsonObjectsFromArray(const std::string& arrayJson)
+{
+    std::vector<std::string> objects;
+
+    bool inString = false;
+    int depth = 0;
+    size_t objStart = std::string::npos;
+
+    for (size_t i = 0; i < arrayJson.size(); ++i) {
+        const char c = arrayJson[i];
+
+        if (c == '"') {
+            size_t backslashes = 0;
+            for (size_t j = i; j > 0 && arrayJson[j - 1] == '\\'; --j) {
+                ++backslashes;
+            }
+            if (backslashes % 2 == 0) {
+                inString = !inString;
+            }
+        }
+
+        if (inString) {
+            continue;
+        }
+
+        if (c == '{') {
+            if (depth == 0) {
+                objStart = i;
+            }
+            ++depth;
+            continue;
+        }
+
+        if (c == '}') {
+            --depth;
+            if (depth == 0 && objStart != std::string::npos) {
+                objects.push_back(arrayJson.substr(objStart, i - objStart + 1));
+                objStart = std::string::npos;
+            }
+        }
+    }
+
+    return objects;
+}
+
+} // namespace
+
+std::string UserData::toJson() const
+{
+    std::ostringstream json;
+    json << "{\n";
+    json << "  \"username\": \"" << escapeJsonString(username) << "\",\n";
+    json << "  \"password\": \"" << escapeJsonString(password) << "\",\n";
+    json << "  \"role\": \"" << escapeJsonString(role) << "\",\n";
+    json << "  \"encrypted\": " << (encrypted ? "true" : "false") << "\n";
+    json << "}";
+    return json.str();
+}
+
+UserData UserData::fromJson(const std::string& jsonText)
+{
+    UserData user{};
+    user.username = extractJsonStringField(jsonText, "username");
+    user.password = extractJsonStringField(jsonText, "password");
+    user.role = extractJsonStringField(jsonText, "role");
+    user.encrypted = extractJsonBoolField(jsonText, "encrypted", false);
+    return user;
+}
 
 Authentication::Authentication(const std::string& usersFilePath)
     : usersFilePath(usersFilePath)
@@ -19,75 +255,42 @@ void Authentication::setEncryptionKey(const std::string& key)
     encryptionKey = key;
 }
 
-std::string Authentication::extractStringValue(const std::string& json, const std::string& key) const
+std::string Authentication::extractStringValue(const std::string& jsonContent, const std::string& key) const
 {
-    std::string searchKey = "\"" + key + "\"";
-    size_t pos = json.find(searchKey);
-    
-    if (pos == std::string::npos) {
-        return "";
-    }
-    
-    pos = json.find(':', pos);
-    if (pos == std::string::npos) {
-        return "";
-    }
-    
-    pos = json.find('"', pos + 1);
-    if (pos == std::string::npos) {
-        return "";
-    }
-    
-    size_t endPos = json.find('"', pos + 1);
-    if (endPos == std::string::npos) {
-        return "";
-    }
-    
-    return json.substr(pos + 1, endPos - pos - 1);
+    return extractJsonStringField(jsonContent, key);
 }
 
 bool Authentication::loadUsers()
 {
+    currentUser = nullptr;
+
     std::ifstream file(usersFilePath);
     if (!file.is_open()) {
         std::cerr << "Nie można otworzyć pliku użytkowników: " << usersFilePath << std::endl;
         return false;
     }
-    
+
     std::ostringstream content;
     content << file.rdbuf();
     std::string jsonContent = content.str();
     file.close();
-    
-    // Parsowanie prostego JSON - szukamy tablicy users
+
     users.clear();
-    
-    size_t arrayStart = jsonContent.find("[");
-    size_t arrayEnd = jsonContent.rfind("]");
-    
-    if (arrayStart == std::string::npos || arrayEnd == std::string::npos) {
-        std::cerr << "Nieprawidłowy format pliku użytkowników" << std::endl;
+
+    const std::string usersArray = findUsersArray(jsonContent);
+    if (usersArray.empty()) {
+        std::cerr << "Nieprawidłowy format pliku użytkowników (brak tablicy users)" << std::endl;
         return false;
     }
-    
-    std::string arrayContent = jsonContent.substr(arrayStart, arrayEnd - arrayStart + 1);
-    
-    // Parsowanie każdego obiektu użytkownika
-    size_t objStart = 0;
-    while ((objStart = arrayContent.find("{", objStart)) != std::string::npos) {
-        size_t objEnd = arrayContent.find("}", objStart);
-        if (objEnd == std::string::npos) break;
-        
-        std::string userJson = arrayContent.substr(objStart, objEnd - objStart + 1);
-        UserData user = UserData::fromJson(userJson);
-        
+
+    const auto objects = splitJsonObjectsFromArray(usersArray);
+    for (const auto& objectJson : objects) {
+        UserData user = UserData::fromJson(objectJson);
         if (!user.username.empty()) {
             users.push_back(user);
         }
-        
-        objStart = objEnd + 1;
     }
-    
+
     std::cout << "Wczytano " << users.size() << " użytkowników" << std::endl;
     return true;
 }
@@ -99,9 +302,9 @@ bool Authentication::saveUsers()
         std::cerr << "Nie można zapisać do pliku użytkowników: " << usersFilePath << std::endl;
         return false;
     }
-    
+
     file << "{\n    \"users\": [\n";
-    
+
     for (size_t i = 0; i < users.size(); ++i) {
         file << "        " << users[i].toJson();
         if (i < users.size() - 1) {
@@ -109,10 +312,10 @@ bool Authentication::saveUsers()
         }
         file << "\n";
     }
-    
+
     file << "    ]\n}\n";
     file.close();
-    
+
     std::cout << "Zapisano " << users.size() << " użytkowników" << std::endl;
     return true;
 }
@@ -133,7 +336,7 @@ std::string Authentication::encryptPassword(const std::string& password)
         // Domyślny klucz jeśli nie ustawiono
         encryptionKey = "BiofeedbackApp2024SecureKey!";
     }
-    
+
     try {
         return Encryption::encrypt(password, encryptionKey);
     } catch (const std::exception& e) {
@@ -147,7 +350,7 @@ std::string Authentication::decryptPassword(const std::string& encryptedPassword
     if (encryptionKey.empty()) {
         encryptionKey = "BiofeedbackApp2024SecureKey!";
     }
-    
+
     try {
         return Encryption::decrypt(encryptedPassword, encryptionKey);
     } catch (const std::exception& e) {
@@ -159,14 +362,14 @@ std::string Authentication::decryptPassword(const std::string& encryptedPassword
 int Authentication::encryptPasswordsIfNeeded(const std::string& key)
 {
     int encryptedCount = 0;
-    
+
     if (key.empty()) {
         std::cerr << "Klucz szyfrujący jest pusty" << std::endl;
         return 0;
     }
-    
+
     encryptionKey = key;
-    
+
     for (auto& user : users) {
         if (!user.encrypted && !user.password.empty()) {
             // Zaszyfruj hasło
@@ -176,26 +379,27 @@ int Authentication::encryptPasswordsIfNeeded(const std::string& key)
             std::cout << "Zaszyfrowano hasło dla użytkownika: " << user.username << std::endl;
         }
     }
-    
+
     // Zapisz zaktualizowane dane
     if (encryptedCount > 0) {
         saveUsers();
     }
-    
+
     return encryptedCount;
 }
 
 bool Authentication::login(const std::string& username, const std::string& password)
 {
     UserData* user = findUser(username);
-    
+
     if (user == nullptr) {
+        currentUser = nullptr;
         std::cout << "Nie znaleziono użytkownika: " << username << std::endl;
         return false;
     }
-    
+
     std::string storedPassword;
-    
+
     if (user->encrypted) {
         // Hasło jest zaszyfrowane - musimy je odszyfrować do porównania
         storedPassword = decryptPassword(user->password);
@@ -203,13 +407,14 @@ bool Authentication::login(const std::string& username, const std::string& passw
         // Hasło nie jest zaszyfrowane - używamy bezpośrednio
         storedPassword = user->password;
     }
-    
+
     if (storedPassword == password) {
         currentUser = user;
         std::cout << "Zalogowano użytkownika: " << username << " (rola: " << user->role << ")" << std::endl;
         return true;
     }
-    
+
+    currentUser = nullptr;
     std::cout << "Nieprawidłowe hasło dla użytkownika: " << username << std::endl;
     return false;
 }
